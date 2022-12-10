763710f89d5a39732bf170c08f52c1b8ff58f74e
azharanowar <azharanowar@gmail.com>
2022-12-11 01:21:50 +0600

    Custom admin login design added to this project

---
+++ resources/views/dashboard/footer.blade.php
@@ -0,0 +1,14 @@
+
+<!--end page main-->
+
+</div>
+<!--end wrapper-->
+
+
+<!--plugins-->
+<script src="{{ asset('dashboardAssets') }}/assets/js/jquery.min.js"></script>
+<script src="{{ asset('dashboardAssets') }}/assets/js/pace.min.js"></script>
+
+
+</body>
+</html>
---
+++ resources/views/dashboard/header.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en" class="minimal-theme">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('dashboardAssets') }}/assets/images/favicon-32x32.png" type="image/png" />
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('dashboardAssets') }}/assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="{{ asset('dashboardAssets') }}/assets/css/bootstrap-extended.css" rel="stylesheet" />
+    <link href="{{ asset('dashboardAssets') }}/assets/css/style.css" rel="stylesheet" />
+    <link href="{{ asset('dashboardAssets') }}/assets/css/icons.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&amp;display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="../../../../../cdn.jsdelivr.net/npm/bootstrap-icons%401.5.0/font/bootstrap-icons.css">
+
+    <!-- loader-->
+    <link href="{{ asset('dashboardAssets') }}/assets/css/pace.min.css" rel="stylesheet" />
+
+    <title>Skodash - Bootstrap 5 Admin Template</title>
+</head>
+
+<body>
+
+<!--start wrapper-->
+<div class="wrapper">
